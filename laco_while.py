d1ddf7c6bbc90de70312579750c25877e4341971
marciorps <marciorps@yahoo.com,.br>
2023-11-24 16:03:21 -0300

    laco_while

---
+++ laco_while.py
@@ -0,0 +1,56 @@
+# # Loop while(laço while)
+# tentativas = 0
+# while tentativas < 3:
+#     print('tente novamente')
+#     tentativas += 1
+
+# # Senha
+# senha = ''
+# while senha != '123456':
+#     senha = input('digite sua senha:')
+# print('bem-vindo!')
+
+# #Receber no do usuário
+# nome = ''
+# while nome == '':
+#     nome = input('digite seu nome:')
+# print(f'bem-vindo {nome}')
+
+
+# #Ver o por do sol as 17:00
+# horario = 0
+# while horario <= 17:
+#     print(horario)
+#     horario += 1
+# print('Hora de ver o por do sol')
+
+# # Contagem regressiva
+
+# contador = 100
+# while contador >= 0:
+#     print(contador)
+#     contador -= 1
+
+
+#Desafio1
+##crie um loop que ira contar de 1 até 120
+
+# contador = 1
+# while contador <= 120:
+#     print(contador)
+#     contador += 1
+
+#desafio2
+## Crie um loop while que ira continuamente pedir ao usuario a senha para entrada, e so ira permitir o programa continuar caso ele digitar a senha "secreta"
+# senha = ''
+# while senha != "123":
+#     senha = input("digite a senha:")
+# print('bem-vindo!')
+
+#Desafio3
+## Crie um loop que conte e imprima na tela o valor em ordem descrecente de 100 para 1
+contador = 100
+while contador >= 0:
+    print(contador)
+    contador -= 1
+
